1a08e2b2f0fdde3df7729080093aec74e47dc12d
Dmitry Lazarev <diman.lazarev@gmail.com>
2017-10-13 20:51:25 +0300

    wifi settings

--- BME280_Webserver.ino
+++ BME280_Webserver.ino
@@ -15,7 +15,7 @@ Adafruit_BME280 bme; // I2C
 
 // Replace with your network details
 const char* ssid = "BOSON24";
-const char* password = "your ssid password";
+const char* password = "kartoshka";
 float h, t, p, pin, dp;
 char temperatureFString[6];
 char dpString[6];
